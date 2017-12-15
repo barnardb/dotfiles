91f2a92d82f5ff1e092251547c47512acd2ffb43
Ben Barnard <barnardb@gmail.com>
2017-12-15 18:24:29 +0100

    Don't try to set apparently non-existing setting

--- macos/apply-settings.sh
+++ macos/apply-settings.sh
@@ -55,7 +55,7 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 # Enable snap-to-grid for icons on the desktop and in other icon views
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+#/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
 # Expand save panel by default
