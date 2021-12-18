c4b19278f2d9e28911ddcc3c37657d50ba599223
Ben Barnard <barnardb@gmail.com>
2021-12-19 00:09:49 +0100

    Remove homebrew perl config I don't need anymore

--- contents/.interactive-shells.d/homebrew.sh
+++ contents/.interactive-shells.d/homebrew.sh
@@ -3,7 +3,3 @@ export PATH="$PATH:/usr/local/sbin"
 
 # Add homebrew npm bin dir to PATH
 export PATH="$PATH:/usr/local/share/npm/bin"
-
-
-# Add homebrew PERL 5 libraries to @INC
-export PERL5LIB="$PERL5LIB:/usr/local/lib/perl5/site_perl"
